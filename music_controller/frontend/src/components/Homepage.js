83d4c2244a138df2e23b47f253fc8f5b4c2015ae
brycenGabaldon <brycengabaldon@gmail.com>
2023-03-06 02:54:41 -0700

    Tutorial 10
    clear session from server
    use callback to refresh homepage

--- music_controller/frontend/src/components/Homepage.js
+++ music_controller/frontend/src/components/Homepage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,7 +10,6 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
-import { ButtonGroup, Button, Grid, Typography } from "@material-ui/core";
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -17,7 +17,9 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
+
   async componentDidMount() {
     fetch("/api/user-in-room")
       .then((response) => response.json())
@@ -39,16 +41,23 @@ export default class HomePage extends Component {
         <Grid item xs={12} align="center">
           <ButtonGroup disableElevation variant="contained" color="primary">
             <Button color="primary" to="/join" component={Link}>
-              Join a room
+              Join a Room
             </Button>
             <Button color="secondary" to="/create" component={Link}>
-              Create a room
+              Create a Room
             </Button>
           </ButtonGroup>
         </Grid>
       </Grid>
     );
   }
+
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -60,13 +69,18 @@ export default class HomePage extends Component {
               return this.state.roomCode ? (
                 <Redirect to={`/room/${this.state.roomCode}`} />
               ) : (
-                this.renderHomePage
+                this.renderHomePage()
               );
             }}
-          ></Route>
+          />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );

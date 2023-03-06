5f50c569273e8f541517345b232b9460f704ae23
brycenGabaldon <brycengabaldon@gmail.com>
2023-03-05 20:48:46 -0700

    downgrade for deprecation

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import Homepage from "./Homepage";
-import CreateRoomPage from "./CreateRoomPage";
-import RoomJoinPage from "./RoomJoinPage";
 
 export default class App extends Component {
   constructor(props) {
--- music_controller/frontend/src/components/Homepage.js
+++ music_controller/frontend/src/components/Homepage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,19 +10,21 @@ import {
   Redirect,
 } from "react-router-dom";
 
-export default class Homepage extends Component {
+export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
     return (
       <Router>
         <Switch>
           <Route exact path="/">
-            <p>this is the homepage</p>
+            <p>This is the home page</p>
           </Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
         </Switch>
       </Router>
     );
